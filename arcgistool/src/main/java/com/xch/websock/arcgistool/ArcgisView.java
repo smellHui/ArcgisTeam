e6f6eab86c2208abae7a279cbea084b4b53e62f0
xch <252243862@qq.com>
2019-05-09 18:03:08 +0800

    部分修改

--- arcgistool/src/main/java/com/xch/websock/arcgistool/ArcgisView.java
+++ arcgistool/src/main/java/com/xch/websock/arcgistool/ArcgisView.java
@@ -1,8 +1,11 @@
 package com.xch.websock.arcgistool;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.view.MapView;
 
 /**
@@ -13,13 +16,28 @@ import com.esri.arcgisruntime.mapping.view.MapView;
 public class ArcgisView extends LinearLayout {
 
     private MapView mapView;
+    private Context ctx;
 
     public ArcgisView(Context context) {
         super(context);
+        initView(context, new MapView(context));
     }
 
-    private void initView(Context ctx) {
-        mapView = new MapView(ctx);
+    public ArcgisView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context, new MapView(context, attrs));
+    }
+
+    public ArcgisView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context, new MapView(context, attrs));
+    }
+
+    private void initView(Context ctx, MapView mapView) {
+        this.mapView = mapView;
+        this.ctx = ctx;
+        ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 47.495052, -121.786863, 12);
+        mapView.setMap(map);
         addView(mapView);
     }
 
